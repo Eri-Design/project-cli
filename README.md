0430995667ef78a442a8447fd0edc330a11ac547
Edwin Cromley <cromleyedwin89@gmail.com>
2019-11-18 12:40:27 -0500

    Initial Files Commit 11.18.2019

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# project-cli
-CLI for creating new projects.
+# Eri Design Project Scaffold
+
+> The Eri Design Project Scaffold is a command line tool to quickly initialize a starter theme or plugin for your new project. It adheres to all Eri Design's best practice coding standards and has been reviewed for security, performance, and scalability. This project is aimed specifically at the work that Eri Design does, but we welcome all community contributions.
+
+## Contributing
+
+If you have an update for the theme or plugin that is generated, please submit those issues or pull requests with the associated repository:
+
+[View the theme repository](https://github.com/Eri-Design/theme-scaffold)
+
+[View the plugin repository](https://github.com/Eri-Design/plugin-scaffold)
+
+## Set Up from Github
+
+1.  Clone the [repository](https://github.com/Eri-Design/project-scaffold) locally
+2.  Run `cd project-scaffold`
+3.  Run `npm install`
+4.  Run `npm link` to make the `create-eri` command global
+
+## Setup from npmjs.com
+
+`npm install @Eri-Design/create-eri -g`
+
+## Project Types
+
+- theme
+- plugin
+
+## Example Usage
+
+`cd <your-project-directory>`
+
+`create-eri theme human-theme-name`
+
+`create-eri plugin human-plugin-name`
+
+`npm install`
+
+## Updating this tool
+
+`create-eri` is in active development. To get the latest, navigate to this folder in your home directory (or wherever else you have it installed) and `git pull`.
+
+## Learn more about the packages used with this project
+
+- [Chalk](https://www.npmjs.com/package/chalk)
+- [Commander](https://www.npmjs.com/package/commander)
+- [FS extra](https://www.npmjs.com/package/fs-extra)
+- [Git clone](https://www.npmjs.com/package/git-clone)
+- [Path](https://www.npmjs.com/package/path)
+- [Replace in file](https://www.npmjs.com/package/replace-in-file)
+
+## Support Level
+
+**Active:** Eri Design is actively working on this, and we expect to continue work for the foreseeable future including keeping tested up to the most recent version of WordPress. Bug reports, feature requests, questions, and pull requests are welcome.
+
+## Like what you see?
+
+<a href="http://Eri-Design.com/contact/"><img src="https://Eri-Designdotcom-wpengine.s3.amazonaws.com/uploads/2016/10/Eri-Design-Github-Banner.png" width="850" alt="Work with us at Eri Design"></a>
---
+++ Scaffold.js
@@ -0,0 +1,269 @@
+// @ts-check
+"use strict";
+
+const chalk = require("chalk");
+const clone = require("git-clone");
+const commander = require("commander");
+const fs = require("fs-extra");
+const packageJson = require("./package.json");
+const path = require("path");
+const replace = require("replace-in-file");
+
+// @TODO: Update this with the final path
+let directoryName = "",
+  projectType = "theme";
+
+/*
+	Set up the CLI
+*/
+
+const program = new commander.Command(packageJson.name)
+  .version(packageJson.version)
+  .arguments("<project-type> <project-directory>")
+  .usage(`${chalk.green("<project-type> <project-directory>")} [options]`)
+  .action((type, name) => {
+    projectType = type.toLowerCase();
+    directoryName = name.toLowerCase();
+  })
+  .allowUnknownOption()
+  .parse(process.argv);
+
+if (
+  "undefined" === typeof projectType ||
+  undefined === packageJson.eri.repos[projectType]
+) {
+  console.error("Please specify the what type of project to create:");
+  console.log(
+    `  ${chalk.cyan(program.name())} ${chalk.green(
+      "<project-type> <project-directory>"
+    )}`
+  );
+  console.log(" Valid project types are 'theme' and 'plugin'.");
+  console.log();
+  console.log("For example:");
+  console.log(
+    `  ${chalk.cyan(program.name())} ${chalk.green("theme my-eri-project")}`
+  );
+  console.log();
+  process.exit(1);
+}
+
+if ("undefined" === typeof directoryName || "" === directoryName) {
+  console.error("Please specify the project directory:");
+  console.log(
+    `  ${chalk.cyan(program.name())} ${chalk.green("<project-directory>")}`
+  );
+  console.log();
+  console.log("For example:");
+  console.log(
+    `  ${chalk.cyan(program.name())} ${chalk.green("my-eri-project")}`
+  );
+  console.log();
+  process.exit(1);
+}
+
+const nameSpaces = directoryName.replace(/-/g, " ");
+const nameCapitalize = nameSpaces.replace(/\b\w/g, l => l.toUpperCase());
+const nameCamelCase = nameCapitalize.replace(/ /g, "");
+const nameUnderscores = directoryName.replace(/-/g, "_");
+const nameUnderscoresUppercase = nameUnderscores.toUpperCase();
+
+// An array of files to remove
+const filesToRemove = ["README.md"];
+
+// An array of directories to remove
+const directoriesToRemove = [".git"];
+
+// Objects of text strings to find and replace
+const textToReplace = [
+  {
+    from: /EriScaffold/g,
+    to: nameCamelCase
+  },
+  {
+    from: /ERI_SCAFFOLD/g,
+    to: nameUnderscoresUppercase
+  },
+  {
+    from: /eri-scaffold/g,
+    to: directoryName
+  },
+  {
+    from: /eri_scaffold/g,
+    to: nameUnderscores
+  },
+  {
+    from: /eri Scaffold/g,
+    to: nameCapitalize
+  }
+];
+
+// Objects of directories that need to be renamed
+const directoriesToRename = [
+  {
+    from: "eri-scaffold",
+    to: directoryName
+  },
+  {
+    from: "eri-plugin-scaffold",
+    to: directoryName
+  },
+  {
+    from: "languages/EriScaffold.pot",
+    to: "languages/" + nameCamelCase + ".pot"
+  }
+];
+
+/*
+	Make sure the directory isn't already there before running the script
+*/
+
+if (fs.existsSync("./" + directoryName)) {
+  console.log(
+    chalk.yellow.bold("✘ Warning: ") +
+      '"' +
+      directoryName +
+      '" directory already exists, please remove it or change the path'
+  );
+
+  // Bail out so you don't delete the directory or error out
+  process.exit(1);
+} else {
+  console.log(chalk.yellow(`Setting up your project. This might take a bit.`));
+}
+
+/*
+	Clone the repo and get to work
+*/
+
+clone(packageJson.eri.repos[projectType], "./" + directoryName, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(chalk.green("✔ Clone Successful"));
+
+    // Delete unnecessary files
+    if (filesToRemove.length) {
+      filesToRemove.forEach(function(file) {
+        // Check to see if the file exists before trying to delete it
+        if (fs.existsSync(directoryName + "/" + file)) {
+          deleteFile(directoryName, file, function() {
+            console.log(chalk.green(`✔ ${file} deleted`));
+          });
+        }
+      });
+    }
+
+    // Delete unnecessary directories
+    if (directoriesToRemove.length) {
+      directoriesToRemove.forEach(function(dir) {
+        if (fs.existsSync(directoryName + "/" + dir)) {
+          deleteDirectory(directoryName + "/" + dir, function() {
+            console.log(chalk.green(`✔ ${dir} deleted`));
+          });
+        }
+      });
+    }
+
+    // Synchronously find and replace text within files
+    textToReplace.forEach(function(text) {
+      try {
+        const changes = replace.sync({
+          files: directoryName + "/**/*.*",
+          from: text.from,
+          to: text.to,
+          encoding: "utf8"
+        });
+
+        console.log(chalk.green.bold(`✔ Modified files: `), changes.join(", "));
+      } catch (error) {
+        console.error("Error occurred:", error);
+      }
+    });
+
+    // Rename directories
+    directoriesToRename.forEach(function(dir) {
+      if (fs.existsSync(directoryName + "/" + dir.from)) {
+        fs.rename(
+          directoryName + "/" + dir.from,
+          directoryName + "/" + dir.to,
+          function(err) {
+            if (err) throw err;
+            console.log(chalk.green("✔ Renamed " + dir.from));
+          }
+        );
+      }
+    });
+  }
+}); // clone()
+
+/**
+ * Delete files
+ * @param {string} dir Directory path
+ * @param {string} [file] Filename to delete (optional, deletes directory if undefined)
+ * @param {Function} [cb] Callback
+ * @returns {Promise}
+ */
+function deleteFile(dir, file, cb) {
+  return new Promise(function(resolve, reject) {
+    var filePath = path.join(dir, file);
+    fs.lstat(filePath, function(err, stats) {
+      if (err) {
+        return reject(err);
+      }
+      if (stats.isDirectory()) {
+        resolve(deleteDirectory(filePath));
+      } else {
+        fs.unlink(filePath, function(err) {
+          if (err) {
+            return reject(err);
+          }
+          resolve();
+        });
+      }
+    });
+
+    if ("function" === typeof cb) {
+      cb.call(this);
+    }
+  });
+} // deleteFile()
+
+/**
+ * Delete directories
+ * @param {string} dir Directory
+ * @param {Function} [cb] Callback
+ * @returns {Promise}
+ */
+function deleteDirectory(dir, cb) {
+  return new Promise(function(resolve, reject) {
+    fs.access(dir, function(err) {
+      if (err) {
+        return reject(err);
+      }
+      fs.readdir(dir, function(err, files) {
+        if (err) {
+          return reject(err);
+        }
+        Promise.all(
+          files.map(function(file) {
+            return deleteFile(dir, file);
+          })
+        )
+          .then(function() {
+            fs.rmdir(dir, function(err) {
+              if (err) {
+                return reject(err);
+              }
+              resolve();
+            });
+          })
+          .catch(reject);
+      });
+    });
+
+    if ("function" === typeof cb) {
+      cb.call(this);
+    }
+  });
+} // deleteDirectory()
